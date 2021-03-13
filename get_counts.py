f4f978c0aaec98a06267007f74eeb1dae25dd984
Bryan McGill <35433155+oibur@users.noreply.github.com>
2021-03-13 10:46:44 -0500

    Update get_counts.py
    edits

--- get_counts.py
+++ get_counts.py
@@ -3,7 +3,7 @@ import requests
 #returns card counts for each pokemon from API
 #requires a txt file with the names of every pokemon
 
-api_key = 'c2eaa76b-c34c-4d3a-8f33-da95a230d9ea'
+api_key = '...'
 api_base_url = 'https://api.pokemontcg.io/v2'
 
 def get_count(name_of_pokemon):
